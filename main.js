75afcb94d0249ce75d4780d85e5473b563563ec9
Florencia Pagella <florencia.pagella94@gmail.com>
2024-10-18 11:24:01 -0300

    localstorage

--- main.js
+++ main.js
@@ -15,6 +15,39 @@ const setDate = () => {
     dateYear.textContent = date.toLocaleString('es', {year: 'numeric'});
 }
 
+//local Storage
+
+const saveTasks = () => {
+    const tasks = [];
+    taskContainer.childNodes.forEach(el => {
+        tasks.push({
+            text: el.firstChild.textContent,
+            done: el.classList.contains('done')
+        });
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const loadTasks = () => {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.forEach(taskData => {
+        const task = document.createElement('div');
+        task.classList.add('task');
+        if (taskData.done) {
+            task.classList.add('done');
+        }
+        task.addEventListener('click', changeTaskState);
+        task.textContent = taskData.text;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Borrar';
+        deleteButton.addEventListener('click', deleteTask);
+        task.appendChild(deleteButton);
+
+        taskContainer.appendChild(task);
+    });
+};
+
 const addNewTask = event => {
     event.preventDefault();
     const {value} = event.target.taskText;
@@ -35,6 +68,9 @@ const addNewTask = event => {
 
     taskContainer.prepend(task);
     event.target.reset();
+// save tasks
+    saveTasks();
+
 };
 
 const changeTaskState = event => {
@@ -47,6 +83,8 @@ const deleteTask = event => {
     event.stopPropagation();
     const task = event.target.parentElement;
     taskContainer.removeChild(task);
+    //save tasks
+    saveTasks();
 };
 
 const order = () => {
@@ -67,3 +105,4 @@ const renderTask = () => {
 
 
 setDate();
+loadTasks();
